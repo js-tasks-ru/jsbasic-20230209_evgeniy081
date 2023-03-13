4f9d615bc0ff0677ba603c8298cf761924079bb2
evgeniy081 <kekeev-e@gaz-is.ru>
2023-03-14 00:31:21 +0300

    1st task

--- 2-module/1-task/index.js
+++ 2-module/1-task/index.js
@@ -1,3 +1,10 @@
 function sumSalary(salaries) {
-  // ваш код...
+  let sum = 0;
+  for (let key in salaries){
+    // Number.isInteger нет в параграфе. А можно ли решить как-то без этого метода и без || ?
+    if (Number.isInteger(salaries[key])){
+      sum += salaries[key];
+    }
+  }
+  return sum;
 }
